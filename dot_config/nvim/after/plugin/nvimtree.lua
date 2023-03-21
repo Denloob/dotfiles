cf586247e04b0632b2f995a612eca0262968d80e
Denloob <magtop@denloob.tk>
2023-03-21 20:39:36 +0200

    Fix nvimtree not starting when opening a folder

--- dot_config/nvim/after/plugin/nvimtree.lua
+++ dot_config/nvim/after/plugin/nvimtree.lua
@@ -1,24 +1,48 @@
--- disable netrw at the very start of your init.lua (strongly advised)
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
 -- set termguicolors to enable highlight groups
 vim.opt.termguicolors = true
 
 require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
+    disable_netrw = true,
+    hijack_cursor = true,
+    hijack_unnamed_buffer_when_opening = true,
+    sync_root_with_cwd = true,
+    sort_by = "case_sensitive",
+    view = {
+        adaptive_size = true,
+        mappings = {
+            list = {
+                { key = "u", action = "dir_up" },
+            },
+        },
     },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
+    renderer = {
+        group_empty = true,
+    },
+    filters = {
+        dotfiles = true,
+    },
+    diagnostics = {
+        enable = true,
+        show_on_dirs = true,
+    },
 })
+
+local function open_nvim_tree(data)
+    -- Opens the nvim-tree if the buffer is a directoy or if it's a [No Name]. --
+    local directory = vim.fn.isdirectory(data.file) == 1
+    local no_name = data.file == "" and vim.bo[data.buf].buftype == ""
+
+    if not directory and not no_name then
+        return
+    end
+
+    -- change to the directory
+    if directory then
+        vim.cmd.cd(data.file)
+    end
+
+    -- open the tree
+    require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
