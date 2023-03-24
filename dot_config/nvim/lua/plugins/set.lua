7feb522fc7daa362a26d66536320bb9232cf0280
Denloob <magtop@denloob.tk>
2023-03-24 20:35:34 +0300

    Change vim to always use clipboard

--- dot_config/nvim/lua/plugins/set.lua
+++ dot_config/nvim/lua/plugins/set.lua
@@ -16,6 +16,8 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
+vim.opt.clipboard="unnamedplus"
+
 -- Having longer updatetime (default is 4000 ms = 4 s) leads to noticeable
 -- delays and poor user experience.
 vim.opt.updatetime = 50
