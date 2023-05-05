84c9029f72435b9f2ce825a86fa2aa2f7744fec6
Denloob <magtop@denloob.tk>
2023-05-05 14:59:06 +0300

    Add nvim/after/plugin to package.path

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,6 +1,7 @@
 if pcall(require, 'impatient') then
     require 'impatient'
 end
+package.path = package.path .. vim.fn.expand(";$XDG_CONFIG_HOME/nvim/after/plugin/?.lua")
 require("plugins.set")
 require("plugins.packer")
 require("plugins.remap")
