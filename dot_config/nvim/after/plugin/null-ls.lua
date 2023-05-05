4ce3b2dcd45281b6727cef8a16e4e310ffac5648
Denloob <magtop@denloob.tk>
2023-05-05 14:20:16 +0300

    Remove stylua

--- dot_config/nvim/after/plugin/null-ls.lua
+++ dot_config/nvim/after/plugin/null-ls.lua
@@ -8,7 +8,6 @@ local cspell_code_action = null_ls.builtins.code_actions.cspell.with({
 
 null_ls.setup({
     sources = {
-        null_ls.builtins.formatting.stylua,
         null_ls.builtins.completion.spell,
         null_ls.builtins.diagnostics.cspell,
         cspell_code_action,
