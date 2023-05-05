244fe34aea4f5d933ed277a09f4b5cb6013c9ad2
Denloob <magtop@denloob.tk>
2023-05-05 14:20:08 +0300

    Change to use a better attach blacklist

--- dot_config/nvim/after/plugin/null-ls.lua
+++ dot_config/nvim/after/plugin/null-ls.lua
@@ -1,29 +1,17 @@
 local null_ls = require("null-ls")
 
-local blacklist = {
-    "NvimTree_1"
-}
 
-local function should_attach(bufnr)
-  local buffer_name = vim.fn.fnamemodify(vim.fn.bufname(bufnr), ':t')
-  -- check if buffer_name is not in the blacklist
-  for _, word in ipairs(blacklist) do
-    if buffer_name == word then
-      return false
-    end
-  end
-
-  return true
-end
+local cspell_code_action = null_ls.builtins.code_actions.cspell.with({
+     disabled_filetypes = { "NvimTree" },
+})
 
 
 null_ls.setup({
-    should_attach = should_attach,
     sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.completion.spell,
         null_ls.builtins.diagnostics.cspell,
-        null_ls.builtins.code_actions.cspell,
+        cspell_code_action,
         null_ls.builtins.diagnostics.credo,
     },
 })
