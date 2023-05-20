179b1f6cd7608ab50435e49da98a223527890a82
Denloob <magtop@denloob.tk>
2023-05-20 10:22:00 +0300

    Change parameter name to indecate it's not being used

--- dot_config/nvim/after/plugin/lsp.lua
+++ dot_config/nvim/after/plugin/lsp.lua
@@ -7,7 +7,7 @@ vim.keymap.set('n', '<leader>dj', vim.diagnostic.goto_next, opts)
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
-local on_attach = function(client, bufnr)
+local on_attach = function(_, bufnr)
     -- Enable completion triggered by <c-x><c-o>
     vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
