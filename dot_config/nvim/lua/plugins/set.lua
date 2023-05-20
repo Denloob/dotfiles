b9e30af9d5cb7f96e3a0b936f3a3279532d5bb22
Denloob <magtop@denloob.tk>
2023-05-20 10:05:39 +0300

    Change header file type from cpp to c

--- dot_config/nvim/lua/plugins/set.lua
+++ dot_config/nvim/lua/plugins/set.lua
@@ -1,3 +1,9 @@
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+    pattern = '*.h',
+    callback = function()
+        vim.bo.filetype = 'c'
+    end
+})
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
