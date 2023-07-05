10f437d0a4c03b171f411236f1ddb58d021bec34
Denloob <magtop@denloob.tk>
2023-07-05 17:43:03 +0300

    Add rustfmt

--- dot_config/nvim/after/plugin/formatter.lua
+++ dot_config/nvim/after/plugin/formatter.lua
@@ -62,5 +62,14 @@ require("formatter").setup {
                 }
             end
         },
+        rust = {
+            function()
+                return {
+                    exe = "rustfmt",
+                    args = { "--emit=stdout" },
+                    stdin = true,
+                }
+            end
+        }
     }
 }
