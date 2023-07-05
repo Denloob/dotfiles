7d434d949811e2bf2ecec68e0b3ef65d6577a8e7
Denloob <magtop@denloob.tk>
2023-07-05 17:44:47 +0300

    Change python documentaion convention to reST

--- dot_config/nvim/after/plugin/neogen.lua
+++ dot_config/nvim/after/plugin/neogen.lua
@@ -1,7 +1,15 @@
 local neogen = require "neogen"
 
 neogen.setup {
-    snippet_engine = "luasnip"
+    snippet_engine = "luasnip",
+    languages = {
+        python = {
+            template = {
+                annotation_convention = "reST"
+            }
+        }
+    },
+    placeholders_hl = "None"
 }
 
 vim.keymap.set("n", "<leader>bd", neogen.generate, {desc = "Generate neogen documentation"})
