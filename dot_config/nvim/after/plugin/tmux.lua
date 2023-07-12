79a6011f4a02d5bc8dafa1ae9f251e18f016ddbb
Denloob <magtop@denloob.tk>
2023-07-12 21:56:02 +0300

    Change tmux integration to use arrows instead of hjkl
    Because I use <C-h> as backspace, the hjkl config didn't let me delete chars while in tmux.
    Maybe in future I will find a better alternative

--- dot_config/nvim/after/plugin/tmux.lua
+++ dot_config/nvim/after/plugin/tmux.lua
@@ -1,4 +1,5 @@
-require 'tmux'.setup {
+local tmux = require("tmux")
+tmux.setup {
     copy_sync = {
         -- disables copy sync.
         enable = false,
@@ -6,17 +7,25 @@ require 'tmux'.setup {
     navigation = {
         -- cycles to opposite pane while navigating into the border
         cycle_navigation = true,
-        -- enables default keybindings (C-hjkl) for normal mode
-        enable_default_keybindings = true,
+        enable_default_keybindings = false,
         -- prevents unzoom tmux when navigating beyond vim border
         persist_zoom = false,
     },
     resize = {
-        -- enables default keybindings (A-hjkl) for normal mode
-        enable_default_keybindings = true,
+        enable_default_keybindings = false,
         -- sets resize steps for x axis
         resize_step_x = 1,
         -- sets resize steps for y axis
         resize_step_y = 1,
     }
 }
+
+vim.keymap.set('n', '<C-Left>', tmux.move_left)
+vim.keymap.set('n', '<C-Right>', tmux.move_right)
+vim.keymap.set('n', '<C-Up>', tmux.move_top)
+vim.keymap.set('n', '<C-Down>', tmux.move_bottom)
+
+vim.keymap.set('n', '<M-Left>', tmux.resize_left)
+vim.keymap.set('n', '<M-Right>', tmux.resize_right)
+vim.keymap.set('n', '<M-Up>', tmux.resize_top)
+vim.keymap.set('n', '<M-Down>', tmux.resize_bottom)
