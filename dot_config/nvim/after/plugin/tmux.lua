ba69ac0db59df35f5e73ee7ef8e175a6db642894
Denloob <den@denloob.uk>
2023-07-22 08:21:34 +0300

    Revert "Change tmux integration to use arrows instead of hjkl"
    This reverts commit 79a6011f4a02d5bc8dafa1ae9f251e18f016ddbb.
    Because I no longer use <C-h> as backspace, I no longer need it, and
    hjkl is more comfortable to use.

--- dot_config/nvim/after/plugin/tmux.lua
+++ dot_config/nvim/after/plugin/tmux.lua
@@ -1,5 +1,4 @@
-local tmux = require("tmux")
-tmux.setup {
+require 'tmux'.setup {
     copy_sync = {
         -- disables copy sync.
         enable = false,
@@ -7,25 +6,17 @@ tmux.setup {
     navigation = {
         -- cycles to opposite pane while navigating into the border
         cycle_navigation = true,
-        enable_default_keybindings = false,
+        -- enables default keybindings (C-hjkl) for normal mode
+        enable_default_keybindings = true,
         -- prevents unzoom tmux when navigating beyond vim border
         persist_zoom = false,
     },
     resize = {
-        enable_default_keybindings = false,
+        -- enables default keybindings (A-hjkl) for normal mode
+        enable_default_keybindings = true,
         -- sets resize steps for x axis
         resize_step_x = 1,
         -- sets resize steps for y axis
         resize_step_y = 1,
     }
 }
-
-vim.keymap.set('n', '<C-Left>', tmux.move_left)
-vim.keymap.set('n', '<C-Right>', tmux.move_right)
-vim.keymap.set('n', '<C-Up>', tmux.move_top)
-vim.keymap.set('n', '<C-Down>', tmux.move_bottom)
-
-vim.keymap.set('n', '<M-Left>', tmux.resize_left)
-vim.keymap.set('n', '<M-Right>', tmux.resize_right)
-vim.keymap.set('n', '<M-Up>', tmux.resize_top)
-vim.keymap.set('n', '<M-Down>', tmux.resize_bottom)
