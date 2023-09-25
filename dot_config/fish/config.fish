7049d74e9123c58e1fc57b4cc3d0048498a5a896
Denloob <den@denloob.uk>
2023-09-25 18:52:52 +0300

    Add tmux-sessionizer

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -17,4 +17,5 @@ if status is-interactive
     abbr -a --position command chez chezmoi cd
     abbr -a --position command fs tempfish
     abbr -a --position command sp nvim_scratchpad
+    abbr -a --position command sm tmux-sessionizer
 end
