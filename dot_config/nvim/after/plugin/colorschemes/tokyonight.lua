8f561b530b56a81a3d0607b5825dfa1e128d97f2
Denloob <den@denloob.uk>
2023-12-29 20:02:39 +0200

    Make number line brighter

--- dot_config/nvim/after/plugin/colorschemes/tokyonight.lua
+++ dot_config/nvim/after/plugin/colorschemes/tokyonight.lua
@@ -25,7 +25,9 @@ require("tokyonight").setup({
   --- You can override specific color groups to use other groups or a hex color
   --- function will be called with a ColorScheme table
   ---@param colors ColorScheme
-  on_colors = function(colors) end,
+  on_colors = function(colors)
+      colors.fg_gutter = "#4c5172"
+  end,
 
   --- You can override specific highlights to use other groups or a hex color
   --- function will be called with a Highlights and ColorScheme table
