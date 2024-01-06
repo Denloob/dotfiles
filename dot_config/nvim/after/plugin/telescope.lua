f8777ffdc5df2fa437e8bf552b45664f42665eb4
Denloob <den@denloob.uk>
2024-01-06 07:40:12 +0200

    Add trouble mappings to telescope

--- dot_config/nvim/after/plugin/telescope.lua
+++ dot_config/nvim/after/plugin/telescope.lua
@@ -1,5 +1,18 @@
 local builtin = require('telescope.builtin')
+local telescope = require("telescope")
+
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+local trouble = require("trouble.providers.telescope")
+
+telescope.setup {
+    defaults = {
+        mappings = {
+            i = { ["<c-t>"] = trouble.open_with_trouble },
+            n = { ["<c-t>"] = trouble.open_with_trouble },
+        },
+    },
+}
