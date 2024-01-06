e833ceed90d3daffa8f480185eb8c3f4861949e6
Denloob <den@denloob.uk>
2024-01-06 07:26:12 +0200

    Remove quit-all bindings
    qa! could be very destructive, and running it with an easy keybinding
    could end up in a horrible way.

--- dot_config/nvim/lua/plugins/remap.lua
+++ dot_config/nvim/lua/plugins/remap.lua
@@ -7,8 +7,6 @@ vim.keymap.set("n", "<C-u>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
-vim.keymap.set("n", "<leader>QA", "<cmd>qa!<CR>")
-vim.keymap.set("n", "<leader>qa", "<cmd>qa<CR>")
 vim.keymap.set("n", "<leader>q", "<cmd>q<CR>")
 vim.keymap.set("n", "<leader>Q", "<cmd>q!<CR>")
 vim.keymap.set("n", "<leader>wq", "<cmd>wq<CR>")
