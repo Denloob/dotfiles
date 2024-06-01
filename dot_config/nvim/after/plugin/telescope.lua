311bac575fdc57779f5ad76e94bce5f565e4edd6
Denloob <den@denloob.uk>
2024-06-01 17:15:12 +0300

    Replace a deprecated function with an alternative

--- dot_config/nvim/after/plugin/telescope.lua
+++ dot_config/nvim/after/plugin/telescope.lua
@@ -6,13 +6,13 @@ vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
 vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
 
-local trouble = require("trouble.providers.telescope")
+local trouble = require("trouble.sources.telescope")
 
 telescope.setup {
     defaults = {
         mappings = {
-            i = { ["<c-t>"] = trouble.open_with_trouble },
-            n = { ["<c-t>"] = trouble.open_with_trouble },
+            i = { ["<c-t>"] = trouble.open },
+            n = { ["<c-t>"] = trouble.open },
         },
     },
 }
